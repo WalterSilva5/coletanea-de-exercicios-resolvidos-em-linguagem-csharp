b0573c3bbaa3d561630a7d3c8bdac8d31463207a
Walter Pereira Da Silva <waltersilva5@outlook.com>
2025-01-07 23:41:02 -0300

    Create 4-type-casting.cs

---
+++ 0-basics/4-type-casting.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace TypeConversionApplication {
+  class StringConversion {
+    static void Main(string[] args){
+      int i = 10;
+      float f = 1.4f;
+      double d = 132.2;
+      bool b = true;
+
+      Console.WriteLine(i.ToString());
+      Console.WriteLine(f.ToString());
+      Console.WriteLine(d.ToString());
+      Console.WriteLine(b.ToString());
+    }
+  }
+}
