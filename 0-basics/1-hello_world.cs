e278daa2d7487508fe49c391d3ad0ac0c678b60a
waltersilva5 <waltersilva5@outlook.com>
2025-01-20 10:10:01 -0300

    create exceptions

--- 0-basics/1-hello_world.cs
+++ 0-basics/1-hello_world.cs
@@ -3,7 +3,6 @@ using System;
 namespace HelloWorldApplication {
    class HelloWorld {
       static void Main(string[] args) {
-         /* my first program in C# */
          Console.WriteLine("Hello World");
          Console.ReadKey();
       }
